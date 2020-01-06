a604dc5fbe89e4ae289f262d49218676fdad4e62
Viktor Volisov <vlaselectronics@gmail.com>
2020-01-06 17:40:51 +0300

    Small fix in run.sh  - It didn't run from Windows Subsystem for Linux

--- run.sh
+++ run.sh
@@ -9,13 +9,14 @@
 #export PYTHONWARNINGS="ignore:Unverified HTTPS request"
 
 # Setup
-VENV_DIR=venv
-if [[ ! (-f "$VENV_DIR/bin/activate" && -f "$VENV_DIR/bin/python3") ]]; then
-  virtualenv $VENV_DIR
+VENV_DIR=.venv
+if [[ ! (-f "$VENV_DIR" && -f "$VENV_DIR/bin/activate" && -f "$VENV_DIR/bin/python3") ]]; then
+  python3 -m pip install --user --upgrade virtualenv
+  python3 -m virtualenv $VENV_DIR
 fi
 
 if [[ -f "$VENV_DIR/bin/activate" && -f "$VENV_DIR/bin/python3" ]]; then
-  source ./venv/bin/activate
+  source ./$VENV_DIR/bin/activate
   if [[ ! $(pip3 list | grep moviepy) ]]; then
     pip3 install moviepy
   fi
